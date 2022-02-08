47d91863f4d4a0fc33592f130059904fdcbafb9a
Büşra <busrarigun@gmail.com>
2022-02-09 00:05:59 +0300

    Header component created

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { View,Text, StyleSheet } from "react-native";
+import colors from "../consts/color";
+
+class Header extends React.Component{
+    render(){
+        return(
+        <View style={styles.header}>
+          <Text style={styles.headerText}> Todo App</Text>
+        </View>
+        )
+    }
+}
+
+const styles=StyleSheet.create({
+    header:{
+        width:"100%",
+        height:80,
+        backgroundColor:colors.headerBackground, 
+        justifyContent:'center',
+        paddingHorizontal:"5%"
+    },
+    headerText:{
+        color:colors.headerText,
+        fontSize:20,
+        fontWeight:"bold",
+        
+    }
+})
+
+export default Header;

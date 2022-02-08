81527b9f396aeeb6bb54ef398ad8ffec53ef95bf
Büşra <busrarigun@gmail.com>
2022-02-09 00:06:50 +0300

    color const created

---
+++ src/consts/color.js
@@ -0,0 +1,14 @@
+
+const colors={
+    headerBackground:"#5F27CD",
+    headerText:"white",
+    statusBarBackground:"341F97",
+    statusBarStyle:"light-content",
+    inputBackground:"lightgrey",
+    buttonBackground:"#10AC84",
+    buttonText:"white",
+    todoCircle:"grey",
+    todoCircleFinished:"#10AC84"
+}
+
+export default colors;
